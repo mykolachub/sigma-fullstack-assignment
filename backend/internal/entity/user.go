139a5a12e50970513f45a692fb499cbe63910f91
Mykola Chub <mykolachubb@gmail.com>
2024-01-12 14:19:32 +0200

    feat: user entity, response, request structs implemented

--- backend/internal/entity/user.go
+++ backend/internal/entity/user.go
@@ -1,1 +1,17 @@
 package entity
+
+import "sigma-test/internal/response"
+
+type User struct {
+	ID       string
+	Email    string
+	Password string
+}
+
+func (u *User) ToResponse() response.User {
+	return response.User{
+		ID:       u.ID,
+		Email:    u.Email,
+		Password: u.Password,
+	}
+}
--- backend/internal/request/user.go
+++ backend/internal/request/user.go
@@ -1,1 +1,19 @@
 package request
+
+import (
+	"sigma-test/internal/entity"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (u User) ToEntity() entity.User {
+	return entity.User{
+		ID:       u.ID,
+		Email:    u.Email,
+		Password: u.Password,
+	}
+}
